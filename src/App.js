20c16f2e5dce4a538f1fa25eba6ac6e02d9843df
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-02-14 22:36:22 +0100

    routing configuration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-     Hello
-    </div>
+    <Router>
+        <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/logowanie" element={ <Home /> } />
+            <Route path="/rejestracja" element={ <Home /> } />
+            <Route path="/wylogowano" element={ <Home /> } />
+        </Routes>
+    </Router>
   );
 }
 
