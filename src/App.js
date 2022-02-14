5175644c56146fbcd6bc262e5ad16ea45459d2c4
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-02-15 00:22:27 +0100

    header navigation buttons basic look done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
+import LoginView from "./components/LoginView";
+import SignUpView from "./components/SignUpView";
 
 function App() {
   return (
     <Router>
         <Routes>
             <Route path="/" element={ <Home /> } />
-            <Route path="/logowanie" element={ <Home /> } />
-            <Route path="/rejestracja" element={ <Home /> } />
+            <Route path="/logowanie" element={ <LoginView /> } />
+            <Route path="/rejestracja" element={ <SignUpView /> } />
             <Route path="/wylogowano" element={ <Home /> } />
         </Routes>
     </Router>
