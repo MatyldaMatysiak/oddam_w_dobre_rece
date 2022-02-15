dd760ddaacd31acb8d616c824d430e739381da26
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-02-15 02:40:39 +0100

    header layout and styling - not done yet

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,12 +1,32 @@
 import React from "react";
 import LoginRegister from "./LoginRegister";
 import Navigation from "./Navigation";
+import separator from "../assets/Decoration.svg"
+import {Link} from "react-router-dom";
+import heroImage from "../assets/HeroImage@2x.png"
 
 export default function HomeHeader() {
     return (
-        <>
-            <LoginRegister/>
-            <Navigation/>
-        </>
+        <header className="headerContainer">
+            <div className="navBox">
+                <LoginRegister/>
+                <Navigation/>
+            </div>
+            <div className="heroContainer">
+                <img className="heroImage" src={heroImage} alt=""/>
+                <div className="header__info">
+                    <h2 className="welcome-text">Zacznij pomagać!<br/>Oddaj niechciane rzeczy w zaufane ręce</h2>
+                    <img className="separator" src={separator} alt="separator"/>
+                    <Link to="/logowanie">
+                        <button className="btn btn-active btn-big">ODDAJ<br/>RZECZY</button>
+                    </Link>
+                    <Link to="/logowanie">
+                        <button className="btn btn-active btn-big">ZORGANIZUJ<br/>ZBIÓRKĘ</button>
+                    </Link>
+                </div>
+            </div>
+
+
+        </header>
     )
 }
