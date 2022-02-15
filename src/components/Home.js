858cf23d2091b792ada61163474878c05b550f35
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-02-15 14:39:07 +0100

    Three columns section done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeHeader from "./HomeHeader";
+import HomeThreeColumns from "./HomeThreeColumns";
 
 export default function Home() {
     return (
         <>
             <HomeHeader/>
+            <HomeThreeColumns/>
         </>
     )
 }
