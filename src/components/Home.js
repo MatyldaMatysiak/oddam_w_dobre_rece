5aa387ca13d171b74aeef7ffabe4179353273984
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-02-16 16:10:36 +0100

    about section done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomeHeader from "./HomeHeader";
 import HomeThreeColumns from "./HomeThreeColumns";
 import HomeSimpleSteps from "./HomeSimpleSteps";
+import HomeAbout from "./HomeAbout";
 
 export default function Home() {
     return (
@@ -9,6 +10,7 @@ export default function Home() {
             <HomeHeader/>
             <HomeThreeColumns/>
             <HomeSimpleSteps/>
+            <HomeAbout/>
         </>
     )
 }
