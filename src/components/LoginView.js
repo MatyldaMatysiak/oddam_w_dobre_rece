6f60d63e7e98ee0f232a85965f49d64c5b1f0f3b
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-02-19 00:16:03 +0100

    jumping form because of loading image problem solved

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import LoginRegister from "./LoginRegister";
 import Navigation from "./Navigation";
 import separator from "../assets/Decoration.svg"
+import {HashLink} from "react-router-hash-link";
 
 export default function LoginView() {
+    const [loader, setLoader] = useState("loading")
+
     return (
         <section className="login">
             <div className="loginRegister__navBox">
@@ -12,23 +15,24 @@ export default function LoginView() {
             </div>
             <form className="loginRegister__form">
                 <h2 className="form__title">Zaloguj się</h2>
-                <img src={separator} alt="separator" className="separator"/>
-                <div className="form__inputs">
-                    <div className="inputs__email inputs">
-                        <label htmlFor="email" className="email-label">Email</label>
-                        <input type="text" id="email" name="email"/>
+                <img src={separator} alt="separator" className="separator" onLoad={() => setLoader('loaded')}/>
+                {loader === 'loaded' ? <>
+                    <div className="form__inputs">
+                        <div className="inputs__email inputs">
+                            <label htmlFor="email" className="email-label">Email</label>
+                            <input type="text" id="email" name="email"/>
+                        </div>
+                        <div className="inputs__password inputs">
+                            <label htmlFor="password" className="password-label">Hasło</label>
+                            <input type="text" id="password" name="password"/>
+                        </div>
                     </div>
-                    <div className="inputs__password inputs">
-                        <label htmlFor="password" className="password-label">Hasło</label>
-                        <input type="text" id="password" name="password"/>
+                    <div className="form__buttons">
+                        <HashLink to="/rejestracja"><button className="btn">Załóż konto</button></HashLink>
+                        <button className="btn btn-active">Zaloguj się</button>
                     </div>
-                </div>
-                <div className="form__buttons">
-                    <button className="btn">Załóż konto</button>
-                    <button className="btn btn-active">Zaloguj się</button>
-                </div>
+                </> : <></>}
             </form>
-
         </section>
     )
 }
--- src/components/SignUpView.js
+++ src/components/SignUpView.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import LoginRegister from "./LoginRegister";
 import Navigation from "./Navigation";
 import separator from "../assets/Decoration.svg"
+import {HashLink} from "react-router-hash-link";
 
 export default function SignUpView() {
+    const [loader, setLoader] = useState("loading")
+
     return (
         <section className="Register">
             <div className="loginRegister__navBox">
@@ -12,25 +15,29 @@ export default function SignUpView() {
             </div>
             <form className="loginRegister__form">
                 <h2 className="form__title">Załóż konto</h2>
-                <img src={separator} alt="separator" className="separator"/>
-                <div className="form__inputs">
-                    <div className="inputs__email inputs">
-                        <label htmlFor="email" className="email-label">Email</label>
-                        <input type="text" id="email" name="email"/>
+                <img src={separator} alt="separator" className="separator" onLoad={() => setLoader('loaded')}/>
+                {loader === 'loaded' ? <>
+                    <div className="form__inputs">
+                        <div className="inputs__email inputs">
+                            <label htmlFor="email" className="email-label">Email</label>
+                            <input type="text" id="email" name="email"/>
+                        </div>
+                        <div className="inputs__password inputs">
+                            <label htmlFor="password" className="password-label">Hasło</label>
+                            <input type="text" id="password" name="password"/>
+                        </div>
+                        <div className="inputs__passwordRepeat inputs">
+                            <label htmlFor="passwordRepeat" className="password-label">Powtórz hasło</label>
+                            <input type="text" id="passwordRepeat" name="passwordRepeat"/>
+                        </div>
                     </div>
-                    <div className="inputs__password inputs">
-                        <label htmlFor="password" className="password-label">Hasło</label>
-                        <input type="text" id="password" name="password"/>
+                    <div className="form__buttons">
+                        <HashLink to="/logowanie"><button className="btn">Zaloguj się</button></HashLink>
+                        <button className="btn btn-active">Załóż konto</button>
                     </div>
-                    <div className="inputs__passwordRepeat inputs">
-                        <label htmlFor="passwordRepeat" className="password-label">Powtórz hasło</label>
-                        <input type="text" id="passwordRepeat" name="passwordRepeat"/>
-                    </div>
-                </div>
-                <div className="form__buttons">
-                    <button className="btn">Zaloguj się</button>
-                    <button className="btn btn-active">Załóż konto</button>
-                </div>
+                </> : <></>}
+
+
             </form>
 
         </section>
