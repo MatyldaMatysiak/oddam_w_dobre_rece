30e18648336238f77ec1f71a32fe753af24f382b
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-02-21 15:22:00 +0100

    step one layout done

--- src/components/GiveAwayForm.js
+++ src/components/GiveAwayForm.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 import StepOne from "./StepOne";
+import bg from "../assets/Background-Form.jpg"
 
 export default function GiveAwayForm() {
     return (
         <>
             <section className="giveAwayForm">
                 <Routes>
-                    <Route path="krok-1" element={ <StepOne /> } />
+                    <Route path="krok-1" element={ <StepOne bg={bg}/> } />
                     <Route path="krok-2" element={ <StepOne /> } />
                     <Route path="krok-3" element={ <StepOne /> } />
                     <Route path="krok-4" element={ <StepOne /> } />
