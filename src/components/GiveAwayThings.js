fafad56c520588ee56e58c095f2cdb7b688c18a9
Matylda Matysiak <84190830+MatyldaMatysiak@users.noreply.github.com>
2022-02-25 19:41:39 +0100

    give away header done

--- src/components/GiveAwayThings.js
+++ src/components/GiveAwayThings.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeContact from "./HomeContact";
 import GiveAwayForm from "./GiveAwayForm";
+import GiveAwayHeader from "./GiveAwayHeader";
 
 export default function GiveAwayThings() {
     return (
         <>
+            <GiveAwayHeader />
             <GiveAwayForm />
             <HomeContact />
         </>
